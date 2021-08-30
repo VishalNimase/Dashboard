966b9d5d180cec5cff2b607b0d4fd6cf9e774d53
Shrikant Hande <handeshrikant763@gmail.com>
2021-08-30 20:40:03 +0530

    contact us

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -3,14 +3,14 @@
 const ContactUs = () => {
     return (
         <>
-        <h1> Contact Us</h1>
+        <h1> Get in  touch</h1>
         <div>
             <h2>
-            Name – Mr Umesh Dhamale<br></br>
-	Address – Chandraneel, Famous Chok, New Sangvi, Pune<br></br>
-	Phone – 9145010803<br></br>
-	Email id - indraneel.technologies@gmail.com 
-</h2>
+            CONTACT US<br></br></h2>
+            <br></br>
+	<h3>Phone: 9145010803</h3>
+	<h3>Email id: indraneel.technologies@gmail.com </h3>
+
         </div>
         </>
     );
