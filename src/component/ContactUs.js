3fd245c787efb440402cf6d8cd04f7f4850ed37f
Shrikant Hande <handeshrikant763@gmail.com>
2021-08-30 20:42:09 +0530

    contact us

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -7,6 +7,7 @@ const ContactUs = () => {
         <div>
             <h2>
             CONTACT US<br></br></h2>
+            <h2>Indraneel Technologies</h2>
             <br></br>
 	<h3>Phone: 9145010803</h3>
 	<h3>Email id: indraneel.technologies@gmail.com </h3>
